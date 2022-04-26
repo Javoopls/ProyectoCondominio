44a60611ebac063ee42b8854d995a7251e053a73
rsep <78937557+rsep@users.noreply.github.com>
2022-04-25 20:52:47 -0400

    .

--- core/models.py
+++ core/models.py
@@ -1,6 +1,3 @@
 from django.db import models
 
-# Create your models here.
-# class Espacio(models.Model):
-#     nombre = models.CharField(max_length=50, verbose_name="Nombre")
-#     precio = models.IntegerField(blank=True, null=True, verbose_name="Precio")
+
